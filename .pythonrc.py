a7f9903107106b490fd10786bb23117d8cc55e22
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2014-05-02 20:50:00 +0400

    Turn off automatic tab completion in Python 3.4

--- .pythonrc.py
+++ .pythonrc.py
@@ -5,18 +5,20 @@ try:
 except ImportError:
     # Fallback to the stdlib readline completer if it is installed.
     # Taken from http://docs.python.org/2/library/rlcompleter.html
-    # print("Jedi is not available")
-    # Installed by default in Python >= 3.4
     try:
         import readline
         import rlcompleter
         readline.parse_and_bind("tab: complete")
         del readline, rlcompleter
     except ImportError:
-        # print("Readline is not available")
         pass
 
 # Import some frequently used modules
 import os
-import sys
 import re
+import sys
+
+# Disable standard hook in Python >= 3.4
+if sys.version_info >= (3, 4):
+    del sys.__interactivehook__ 
+
